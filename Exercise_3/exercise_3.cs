69f4a498758f0207680fd7a5a069d4e2c410ce03
Candlejack <garouspeak@gmail.com>
2022-08-11 17:54:43 +0300

    exercise 3 is done

---
+++ Exercise_3/exercise_3.cs
@@ -0,0 +1,11 @@
+﻿int[] week = {1, 2, 3, 4, 5, 6, 7};
+Console.WriteLine("Введите порядковый номер дня недели (начиная с понедельника): ");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i < week[5])
+{
+    Console.WriteLine("Это будний день :(");
+}
+else
+{
+    Console.WriteLine("Это выходной! :)");
+}
