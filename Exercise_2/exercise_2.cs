422b71b3a51b5849fe9ff9d964377ab6abb9294c
Candlejack <garouspeak@gmail.com>
2022-08-11 17:32:44 +0300

    exercise 2 is done

---
+++ Exercise_2/exercise_2.cs
@@ -0,0 +1,12 @@
+﻿char[] array = Console.ReadLine().ToCharArray();
+int i = 3;
+if(i > array.Length)
+{
+     Console.Write("Третья цифра отсутствует!");
+    
+}
+else
+{
+    Console.Write("Третья цифра введенного вами числа - ");
+    Console.Write(array[2]);
+}
